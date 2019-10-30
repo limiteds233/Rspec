08c034cf4da6a7c8c6265b4a7df30e75aa709839
limiteds233 <ss2333_1@mail.ru>
2019-10-30 09:47:59 +0300

    add test for car

--- car.rb
+++ car.rb
@@ -1,3 +1,23 @@
 class Car
-    
+    MILES_PER_GALLON = 20
+
+    attr_reader :fuel
+
+    def initialize
+        @fuel = 0
+    end
+
+    def add_fuel amount
+        @fuel += amount
+    end
+
+    #как далеко можем проехать
+        def range
+           0
+        end
+
 end
+
+car = Car.new
+car.add_fuel 10
+puts "Range is #{car.range} miles" #мы сможем проехать миль
